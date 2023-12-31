ca7898289f5966b513ab25996441743ecf0cffe2
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-31 15:54:58 +0100

    Adding a thread pool to the stdlib

---
+++ threads/thread_pool.go
@@ -0,0 +1,97 @@
+package threads
+
+import (
+	"io"
+	"sync"
+
+	"github.com/bylexus/go-stdlib/log"
+)
+
+type ThreadId int
+type JobFn func(threadId ThreadId)
+type Job interface {
+	Run(threadId ThreadId)
+}
+
+type ThreadPool struct {
+	threads     int
+	job_channel chan JobFn
+	wait_group  *sync.WaitGroup
+	logger      *log.SeverityLogger
+	isRunning   bool
+	lock        *sync.Mutex
+}
+
+// NewThreadPool creates a new ThreadPool with the specified number of threads (goroutines).
+// Optionally, a log.SeverityLogger can be passed in. If not provided, a null logger will be used.
+func NewThreadPool(threads int, logger *log.SeverityLogger) ThreadPool {
+	if logger == nil {
+		l := log.NewSeverityLogger(io.Discard)
+		logger = &l
+	}
+	tp := ThreadPool{
+		threads:     threads,
+		job_channel: make(chan JobFn, threads),
+		wait_group:  &sync.WaitGroup{},
+		logger:      logger,
+		isRunning:   false,
+		lock:        &sync.Mutex{},
+	}
+	return tp
+}
+
+// Starts the number of configured worker routines in the pool
+//
+// If the pool is not started, enqueued jobs are not processed.
+func (tp *ThreadPool) Start() {
+	tp.lock.Lock()
+	defer tp.lock.Unlock()
+	if !tp.isRunning {
+		tp.logger.Debug("Starting thread pool with %d threads", tp.threads)
+		tp.isRunning = true
+		for i := 1; i <= tp.threads; i++ {
+			tp.wait_group.Add(1)
+			go tp.workerRun(ThreadId(i))
+		}
+	}
+}
+
+// Enqueues a job by providing a JobFn function.
+func (tp *ThreadPool) AddJobFn(jobFn JobFn) {
+	tp.lock.Lock()
+	if tp.isRunning {
+		// unlock before enqueuing - the channel could block,
+		// but we don't want the lock to be held for too long:
+		tp.lock.Unlock()
+		tp.job_channel <- jobFn
+	} else {
+		tp.lock.Unlock()
+	}
+}
+
+func (tp *ThreadPool) AddJob(job Job) {
+	tp.AddJobFn(job.Run)
+}
+
+func (tp *ThreadPool) Shutdown() {
+	if tp.isRunning {
+		tp.logger.Debug("Shutting down thread pool")
+		tp.lock.Lock()
+		tp.isRunning = false
+		tp.lock.Unlock()
+		close(tp.job_channel)
+		tp.wait_group.Wait()
+		tp.logger.Debug("Thread pool stopped")
+	}
+}
+
+func (tp *ThreadPool) workerRun(threadId ThreadId) {
+	tp.logger.Debug("Thread %d: Started", threadId)
+	for job := range tp.job_channel {
+		tp.logger.Debug("Thread %d: Working ...", threadId)
+		job(threadId)
+		tp.logger.Debug("Thread %d: Done with job ...", threadId)
+	}
+	tp.wait_group.Done()
+	tp.logger.Debug("Thread %d: Stopped", threadId)
+}
---
+++ threads/thread_pool_test.go
@@ -0,0 +1,72 @@
+package threads_test
+
+import (
+	"io"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/bylexus/go-stdlib/log"
+	"github.com/bylexus/go-stdlib/threads"
+)
+
+func TestThreadPoolWithJobFn(t *testing.T) {
+	logger := log.NewSeverityLogger(io.Discard)
+	tp := threads.NewThreadPool(3, &logger)
+	results := make([]int, 0)
+	mu := sync.Mutex{}
+
+	tp.Start()
+	for i := 1; i <= 3; i++ {
+		localI := i
+		tp.AddJobFn(func(id threads.ThreadId) {
+			mu.Lock()
+			defer mu.Unlock()
+			results = append(results, localI)
+			time.Sleep(time.Millisecond * 100)
+		})
+	}
+	tp.Shutdown()
+	if len(results) != 3 {
+		t.Errorf("Expected 3 results, got %d", len(results))
+	}
+	if results[0] == results[1] || results[1] == results[2] {
+		t.Errorf("Expected different results, got same")
+	}
+}
+
+type TestJob struct {
+	res *chan int
+}
+
+func (j *TestJob) Run(threadId threads.ThreadId) {
+	*j.res <- int(threadId)
+	time.Sleep(time.Millisecond * 100)
+}
+
+func TestThreadPoolWithJobStruct(t *testing.T) {
+	logger := log.NewSeverityLogger(io.Discard)
+	tp := threads.NewThreadPool(3, &logger)
+	resultChan := make(chan int, 3)
+	results := make([]int, 0)
+
+	tp.Start()
+	for i := 1; i <= 3; i++ {
+		job := TestJob{
+			res: &resultChan,
+		}
+		tp.AddJob(&job)
+	}
+	tp.Shutdown()
+	close(resultChan)
+	for r := range resultChan {
+		results = append(results, r)
+	}
+
+	if len(results) != 3 {
+		t.Errorf("Expected 3 results, got %d", len(results))
+	}
+	if results[0] == results[1] || results[1] == results[2] {
+		t.Errorf("Expected different results, got same")
+	}
+}
